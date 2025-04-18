66f44e4c45085ddc2b6e3b37c8ea878720866888
lmz <1611944656@qq.com>
2025-04-18 07:34:43 -0700

    2025-04-18_V1.0

---
+++ 01_环境搭建/install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install uuid uuid-dev zlib1g-dev liblz-dev liblzo2-2 liblzo2-dev lzop \
+git-core curl u-boot-tools mtd-utils android-tools-fsutils openjdk-8-jdk device-tree-compiler \
+gdisk m4 libz-dev git gnupg flex bison gperf libsdl1.2-dev libesd-java libwxgtk3.0-dev \
+squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev pngcrush schedtool \
+libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev \
+lib32ncurses5-dev lib32readline-dev gcc-multilib libswitch-perl libssl-dev unzip \
+zip liblz4-tool repo git ssh make gcc libssl-dev liblz4-tool vim expect \
+g++ patchelf chrpath gawk texinfo chrpath diffstat binfmt-support \
+qemu-user-static live-build bison flex fakeroot cmake gcc-multilib g++-multilib \
+unzip device-tree-compiler python-pip libncurses5-dev rsync subversion \
+sed make binutils  build-essential  gcc  g++  wget python libncurses5 bzr cvs git mercurial \
+patch gzip bzip2 perl tar cpio unzip rsync file bc wget qemu-user-static live-build -y
+
